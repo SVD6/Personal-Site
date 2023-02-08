7b31d71772ffa8f208d6ff38abf11aadcec43ae6
Sai Vikranth Desu <vikranthdesu@gmail.com>
2023-02-08 11:40:57 -0500

    Remove excess metadata.

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Sai Vikranth Desu";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "SVD";
 
 export default function Layout({ children, home }) {
     return (
@@ -14,16 +14,9 @@ export default function Layout({ children, home }) {
                 <link rel="icon" href="/favicon.ico" />
                 <meta
                     name="description"
-                    content="Learn how to build a personal website using Next.js"
-                />
-                <meta
-                    property="og:image"
-                    content={`https://og-image.vercel.app/${encodeURI(
-                        siteTitle
-                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                    content="Sai Vikranth's Personal Website"
                 />
                 <meta name="og:title" content={siteTitle} />
-                <meta name="twitter:card" content="summary_large_image" />
             </Head>
             <header className={styles.header}>
                 {home ? (
